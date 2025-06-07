026fb6a586be79500c255701925c4b27082391cd
Lains <no@such.email>
2025-06-07 17:37:01 +0800

    更新类

--- vndb_classes.py
+++ vndb_classes.py
@@ -49,12 +49,3 @@ class Character():
         return self.name
     
 
-""" 
-game = VisualNovel("七ヶ音学園 -旅行部-","v32820","Main character Kei Tokisaka, is a second year student attending a travel club. He is an ordinary student, but he has a secret that he can't tell anyone about. He adores club members who are all beautiful girls: the beautiful head of the club, the idol girl, the honor student's childhood friend, and cute younger students. The life of the club is filled with excitement, anxiety, humor and romance.")
-A = Character('Main','ZZZ','Amy')
-B = Character('Main12','ZZ2Z','Asmy')
-C = Character('Maqin','ZZqZ','Amqy')
-game.add_Char(A)
-game.add_Chars([B,C])
-game.get_all_Chars()
-print(game.title,game.id,game.description) """
